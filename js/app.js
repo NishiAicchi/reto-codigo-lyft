e98ab8be006c0126744b6ca8775e819f1017ed72
NishiAicchi <ai.espichan@gmail.com>
2017-12-29 06:04:20 -0500

    implementando verify

--- js/app.js
+++ js/app.js
@@ -5,5 +5,5 @@ $(document).ready(function() {
 
   var $logo = $('#logo');
   $logo.animate({ height: '100px', opacity: '.2'}, 1000);
-  $logo.animate({ height: '160px', opacity: '.6'}, 1000);
+  $logo.animate({ height: '200px', opacity: '.6'}, 1000);
 });
