8d3a5b7a063765417c7aeb15087e0c355e902f8b
NishiAicchi <ai.espichan@gmail.com>
2017-12-29 00:37:40 -0500

    cambiando el framework a bootstrap/ reiniciando estructuras HTML/ añadiendo animación a logo central

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  setTimeout(function() {
+    window.location.href = 'views/home.html';
+  }, 3000);
+
+  var $logo = $('#logo');
+  $logo.animate({ height: '100px', opacity: '.2'}, 1000);
+  $logo.animate({ height: '200px', opacity: '.6'}, 1000);
+});
