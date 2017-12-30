590ac3543e073b7c88792653345b370e27b0aae5
NishiAicchi <ai.espichan@gmail.com>
2017-12-30 00:10:34 -0500

    añadiendo estilos y views

--- js/app.js
+++ js/app.js
@@ -4,6 +4,9 @@ $(document).ready(function() {
   }, 3000);
 
   var $logo = $('#logo');
-  $logo.animate({ height: '100px', opacity: '.2'}, 1000);
-  $logo.animate({ height: '200px', opacity: '.6'}, 1000);
+  $logo.animate({ height: '100px',
+    opacity: '.2'}, 1000);
+  $logo.animate({ height: '250px', 
+    width: '400px', 
+    opacity: '.6'}, 900);
 });
