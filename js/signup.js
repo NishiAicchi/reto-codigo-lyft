11cf54811e174f0291df32e81255a352b854e541
NishiAicchi <ai.espichan@gmail.com>
2017-12-31 14:18:33 -0500

    añadiendo funcionalidad a vista SIGNUP

---
+++ js/signup.js
@@ -0,0 +1,73 @@
+$(document).ready(function() {
+  // selección de Elementos
+  var $phone = $('#phone-number');
+  var $flags = $('li');
+  var $btn = $('#next-btn');
+
+  // declarando variables
+  var count = 0;
+  var validatePhone = false;
+  var MINCHARACTER = 11;
+
+  // Evento para seleccionar el país
+  $flags.on('click', function(e) {
+    count = $(this).data('option');
+    switch (true) {
+    case count === 0:
+      $('#USA').replaceAll($('#dropdownMenu :first-child'));
+      $phone.val('1');
+      break;
+    case count === 1:
+      $('#BR').replaceAll($('#dropdownMenu :first-child'));
+      $phone.val('55');
+      break;
+    case count === 2:
+      $('#PE').replaceAll($('dropdownMenu :first-child'));
+      $phone.val('51');
+      break;
+    }
+
+    // añadiendo clase para que no cambie el tamaño del botón
+    $('#dropdownMenu').addClass('btn-flag');
+    // habilitando el input
+    $phone.attr('disabled', false);
+    codeLength = $phone.val().length;    
+  });
+
+  // evento para validar el numero telefonico ingresado
+
+  $phone.on('keyup', function(e) {
+    // console.log($(this).val().length);
+    $phoneLength = $(this).val().length;
+    
+    if ($phoneLength === MINCHARACTER) {
+      $phone.attr('disabled', 'disabled');
+      validatePhone = true;
+      activateBtn();
+    } else if ($phoneLength < MINCHARACTER) {
+      validatePhone = false;
+      desactiveBtn();
+    }
+  });
+
+  // Evento para activar boton
+  function activateBtn() {
+    $btn.attr('disabled', false);
+  };
+
+  // Evento para desactivar botón
+  function desactiveBtn() {
+    $btn.attr('disabled', 'disabled');
+  }
+
+  // Evento del botón "NEXT"
+  $btn.on('click', function(e) {
+    e.preventDefault();
+    labCode = Math.floor(Math.random() * 990) + 100;
+    alert('Tu codigo LAB es ' + labCode)
+    //
+    localStorage.code = labCode;
+    localStorage = $phone.val();
+    window.location.href = 'verify.html';
+  });
+});
