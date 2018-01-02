8ba658d3b58491c12fb564343873d87e8e13cb3b
NishiAicchi <ai.espichan@gmail.com>
2018-01-02 14:29:43 -0500

    redireccionando vista COMPLETE a HOME

--- js/complete.js
+++ js/complete.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
   $icon = $('#icon-complete');
   $logo = $('#logo');
 
+  // animacion del icono
   $icon.fadeOut(500).fadeIn(2000);
 
+  // redireccionando a la vista HOME
+  window.location.href = 'home.html';
 });
